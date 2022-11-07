bbb8115e70ed4c4bf8a42550ca3f7043f0e29459
Juan Fernando Galvez Parra <jfgalvezparra@gmail.com>
2022-11-07 13:11:50 -0500

    Correcting https

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -8,7 +8,7 @@ const useFetchData = ({ search }) => {
     useEffect(() => {
         const fetchedData = async () => {
             try{
-            await fetch(`http://openlibrary.org/search.json?q=${search}`)
+            await fetch(`https://openlibrary.org/search.json?q=${search}`)
                 .then(res => res.json())
                 .then(json => setResults(json));
             } catch (error){
